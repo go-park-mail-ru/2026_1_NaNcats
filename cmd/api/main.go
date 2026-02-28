5f444c3a08000596e67c4a43b703b7563c3cbbad
Arslan <arsfarkhitov@gmail.com>
2026-02-28 21:54:45 +0300

    Добавлена заглушка авторизации и реализован запуск сервера в main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/go-park-mail-ru/2026_1_NaNcats/internal/transport/httphandler"
+)
+
+func main() {
+	port := os.Getenv("PORT") // выделенный под сервер порт из окружения
+	if port == "" {
+		port = "8080"
+	}
+
+	http.HandleFunc("/api/register", httphandler.RegisterHandler)
+
+	// добавить роут api/login
+
+	log.Printf("Server is starting on port %s...", port)
+
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
