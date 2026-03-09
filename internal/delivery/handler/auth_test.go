adf9de00cb6b174593d21fa8c91baeb75a65a3a9
Arslan <arsfarkhitov@gmail.com>
2026-03-09 19:05:29 +0300

    Добавлен тест для Login

--- internal/delivery/handler/auth_test.go
+++ internal/delivery/handler/auth_test.go
@@ -16,6 +16,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func TestAuthHandler_Register(t *testing.T) {
@@ -104,5 +105,67 @@ func TestAuthHandler_GetMe(t *testing.T) {
 
 		// код в GetMe при !ok возвращает StatusInternalServerError
 		assert.Equal(t, http.StatusInternalServerError, rec.Code)
+	})
+}
+
+func TestAuthHandler_Login(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockAuthUC := mocks.NewMockAuthUseCase(ctrl)
+
+	authHandler := NewAuthHandler(mockAuthUC)
+
+	t.Run("Успешный вход", func(t *testing.T) {
+		reqBody := LoginRequest{Login: "test@gmail.com", Password: "aboba"}
+
+		var buf bytes.Buffer
+		err := json.NewEncoder(&buf).Encode(reqBody)
+		assert.NoError(t, err)
+
+		req := httptest.NewRequest(http.MethodPost, "/api/auth/login", &buf)
+		rec := httptest.NewRecorder()
+
+		mockUser := domain.User{ID: uuid.New(), Name: "Ivan", Email: "test@mail.ru"}
+		mockSess := domain.Session{ID: uuid.New(), ExpiresAt: time.Now().Add(time.Hour)}
+
+		mockAuthUC.EXPECT().Login(gomock.Any(), gomock.Any()).Return(mockUser, mockSess, nil)
+
+		authHandler.Login(rec, req)
+
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Contains(t, rec.Header().Get("Set-Cookie"), "session_id")
+
+		var resp LoginResponse
+		err = json.NewDecoder(rec.Body).Decode(&resp)
+		assert.NoError(t, err)
+		assert.Equal(t, mockUser.Name, resp.Name)
+	})
+
+	t.Run("Неверный JSON", func(t *testing.T) {
+		req := httptest.NewRequest(http.MethodPost, "/api/auth/login", strings.NewReader("invalid JSON"))
+		rec := httptest.NewRecorder()
+
+		authHandler.Login(rec, req)
+
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+	})
+
+	t.Run("Неверный пароль", func(t *testing.T) {
+		reqBody := LoginRequest{Login: "test@mail.ru", Password: "wrong"}
+		var buf bytes.Buffer
+		json.NewEncoder(&buf).Encode(reqBody)
+
+		req := httptest.NewRequest(http.MethodPost, "/api/auth/login", &buf)
+		rec := httptest.NewRecorder()
+
+		// Программируем UseCase вернуть ошибку
+		mockAuthUC.EXPECT().
+			Login(gomock.Any(), gomock.Any()).
+			Return(domain.User{}, domain.Session{}, bcrypt.ErrMismatchedHashAndPassword)
+
+		authHandler.Login(rec, req)
+
+		assert.Equal(t, http.StatusBadRequest, rec.Code) // Или StatusUnauthorized, смотря что в хендлере
 	})
 }
