a0a24639a7238670e2e962225fce7a310096072a
Arslan <arsfarkhitov@gmail.com>
2026-04-16 20:21:28 +0300

    Добавлена возможность оборачивать контекст к ошибке

--- shared/pkg/errutil/error.go
+++ shared/pkg/errutil/error.go
@@ -3,21 +3,37 @@ package errutil
 import "google.golang.org/grpc/codes"
 
 type domainError struct {
-	Message string
-	Code    codes.Code
+	message string
+	code    codes.Code
+	cause   error
 }
 
 func (e domainError) Error() string {
-	return e.Message
+	return e.message
 }
 
 func (e domainError) GRPCStatus() codes.Code {
-	return e.Code
+	return e.code
 }
 
+// Создает базовую ошибку (для констант в domain)
 func New(msg string, code codes.Code) domainError {
 	return domainError{
-		Message: msg,
-		Code:    code,
+		message: msg,
+		code:    code,
 	}
+}
+
+// Позволяет добавить контекст к существующей ошибке
+func Wrap(msg string, cause error, code codes.Code) domainError {
+	return domainError{
+		message: msg,
+		cause:   cause,
+		code:    code,
+	}
+}
+
+// Если нужно быстро вернуть ошибку без создания переменной
+func Message(msg string) domainError {
+	return domainError{message: msg, code: codes.Internal}
 }
