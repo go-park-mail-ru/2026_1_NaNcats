d1c17ee4859cca993089414558affb67a6b133c4
KEI3AB <ramazanoff06@yandex.ru>
2026-04-19 17:42:14 +0300

    Изменения в PaymentService: - Добавил метод CreatePayment - Исправил ошибки - Добавил ключи идемпотентности в аргументы - Добавил в структуру usecase клиента заказа для кросс-сервисных GET-запросов

--- services/payment/internal/usecase/payment.go
+++ services/payment/internal/usecase/payment.go
@@ -2,26 +2,28 @@ package payment
 
 import (
 	"context"
-	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/go-park-mail-ru/2026_1_NaNcats/services/payment/internal/domain"
 	"github.com/go-park-mail-ru/2026_1_NaNcats/services/payment/internal/repository"
 	"github.com/go-park-mail-ru/2026_1_NaNcats/shared/pkg/api_clients/yookassa"
+	"github.com/go-park-mail-ru/2026_1_NaNcats/shared/pkg/errutil"
+	"google.golang.org/grpc/codes"
 )
-
-//go:generate mockgen -destination=mocks/payment_mock.go -package=mocks github.com/go-park-mail-ru/2026_1_NaNcats/internal/usecase/payment PaymentUseCase
 
 type OrderClient interface {
 	UpdateOrderStatus(ctx context.Context, paymentID string, status string) error
 }
 
+//go:generate mockgen -destination=mocks/payment_mock.go -package=mocks github.com/go-park-mail-ru/2026_1_NaNcats/internal/usecase/payment PaymentUseCase
 type PaymentUseCase interface {
-	// TODO: добавить метод CreatePayment, сейчас эта логика реализована в order.go в слое usecase, нужно вынести сюда
-	InitiateCardBinding(ctx context.Context, userID int64) (string, error)
+	CreatePayment(ctx context.Context, amount int64, paymentMethodID, idempotencyKey string) (string, string, error)
+	InitiateCardBinding(ctx context.Context, userID int64, idempotencyKey string) (string, error)
 	GetUserCards(ctx context.Context, userID int64) ([]domain.PaymentMethod, error)
-	SetDefaultCard(ctx context.Context, cardID string, userID int64) error
-	DeleteCard(ctx context.Context, cardID string, userID int64) error
+	SetDefaultCard(ctx context.Context, cardID string, userID int64, idempotencyKey string) error
+	DeleteCard(ctx context.Context, cardID string, userID int64, idempotencyKey string) error
+
 	ProcessPaymentMethodWebhook(ctx context.Context, paymentMethod *yookassa.WebhookPaymentMethodObject) error
 	ProcessPaymentWebhook(ctx context.Context, payment *yookassa.WebhookPaymentObject) error
 }
@@ -29,22 +31,58 @@ type PaymentUseCase interface {
 type paymentUseCase struct {
 	paymentRepo    repository.PaymentRepository
 	cacheRepo      repository.PaymentCacheRepository
-	orderRepo      repository.OrderRepository // TODO: удалить
+	orderClient    OrderClient
 	yookassaClient *yookassa.Client
 	returnURL      string
 }
 
-func NewPaymentUseCase(pr repository.PaymentRepository, cr repository.PaymentCacheRepository, or repository.OrderRepository, yc *yookassa.Client, returnURL string) PaymentUseCase {
+func NewPaymentUseCase(pr repository.PaymentRepository, cr repository.PaymentCacheRepository, oc OrderClient, yc *yookassa.Client, returnURL string) PaymentUseCase {
 	return &paymentUseCase{
 		paymentRepo:    pr,
 		cacheRepo:      cr,
-		orderRepo:      or,
+		orderClient:    oc,
 		yookassaClient: yc,
 		returnURL:      returnURL,
 	}
 }
 
-func (p *paymentUseCase) InitiateCardBinding(ctx context.Context, userID int) (string, error) {
+func (p *paymentUseCase) CreatePayment(ctx context.Context, amount int64, paymentMethodID, idempotencyKey string) (string, string, error) {
+	rubles := amount / 1_000_000
+	kopecks := (amount%1_000_000)/10_000 + 100
+	value := strconv.FormatInt(rubles, 10) + "." + strconv.FormatInt(kopecks, 10)[1:]
+
+	paymentRequest := yookassa.CreatePaymentRequest{
+		Amount: yookassa.CreatePaymentRequestAmount{
+			Value:    value,
+			Currency: "RUB",
+		},
+		Capture:           true,
+		SavePaymentMethod: false,
+	}
+
+	paymentRequest.Confirmation = &yookassa.CreatePaymentRequestConfirmation{
+		Type:      "redirect",
+		ReturnURL: p.returnURL,
+	}
+
+	if paymentMethodID != "" {
+		paymentRequest.PaymentMethodID = paymentMethodID
+	}
+
+	paymentResponse, err := p.yookassaClient.CreatePayment(ctx, paymentRequest, idempotencyKey)
+	if err != nil {
+		return "", "", errutil.Wrap("failed to create payment in yookassa", err, codes.Internal)
+	}
+
+	var confirmationURL string
+	if paymentResponse.Confirmation != nil && paymentResponse.Confirmation.Type == "redirect" {
+		confirmationURL = paymentResponse.Confirmation.ConfirmationURL
+	}
+
+	return paymentResponse.ID, confirmationURL, nil
+}
+
+func (p *paymentUseCase) InitiateCardBinding(ctx context.Context, userID int64, idempotencyKey string) (string, error) {
 	req := yookassa.CreatePaymentMethodRequest{
 		Type: "bank_card",
 		Confirmation: &yookassa.PaymentMethodRequestConfirmation{
@@ -53,33 +91,45 @@ func (p *paymentUseCase) InitiateCardBinding(ctx context.Context, userID int) (s
 		},
 	}
 
-	resp, err := p.yookassaClient.CreatePaymentMethod(ctx, req)
+	resp, err := p.yookassaClient.CreatePaymentMethod(ctx, req, idempotencyKey)
 	if err != nil {
-		return "", fmt.Errorf("failed to initiate card binding: %w", err)
+		return "", errutil.Wrap("failed to initiate card binding", err, codes.Internal)
 	}
 
 	if resp.Confirmation == nil || resp.Confirmation.ConfirmationURL == "" {
-		return "", domain.ErrYookassaConfirmationURL
+		return "", errutil.New("empty confirmation url from payment provider", codes.Internal)
 	}
 
 	err = p.cacheRepo.SetPendingBinding(ctx, resp.ID, userID, 15*time.Minute)
 	if err != nil {
-		return "", fmt.Errorf("failed to save pending binding in cache: %w", err)
+		return "", errutil.Wrap("failed to save pending binding in cache", err, codes.Internal)
 	}
 
 	return resp.Confirmation.ConfirmationURL, nil
 }
 
 func (p *paymentUseCase) GetUserCards(ctx context.Context, userID int64) ([]domain.PaymentMethod, error) {
-	return p.paymentRepo.GetByUserID(ctx, userID)
+	methods, err := p.paymentRepo.GetByUserID(ctx, userID)
+	if err != nil {
+		return nil, errutil.Wrap("failed to get user cards", err, codes.Internal)
+	}
+	return methods, nil
 }
 
-func (p *paymentUseCase) SetDefaultCard(ctx context.Context, cardID string, userID int64) error {
-	return p.paymentRepo.SetDefault(ctx, cardID, userID)
+func (p *paymentUseCase) SetDefaultCard(ctx context.Context, cardID string, userID int64, idempotencyKey string) error {
+	err := p.paymentRepo.SetDefault(ctx, cardID, userID)
+	if err != nil {
+		return errutil.Wrap("failed to set default card", err, codes.Internal)
+	}
+	return nil
 }
 
-func (p *paymentUseCase) DeleteCard(ctx context.Context, cardID string, userID int64) error {
-	return p.paymentRepo.Delete(ctx, cardID, userID)
+func (p *paymentUseCase) DeleteCard(ctx context.Context, cardID string, userID int64, idempotencyKey string) error {
+	err := p.paymentRepo.Delete(ctx, cardID, userID)
+	if err != nil {
+		return errutil.Wrap("failed to delete card", err, codes.Internal)
+	}
+	return nil
 }
 
 func (p *paymentUseCase) ProcessPaymentMethodWebhook(ctx context.Context, pm *yookassa.WebhookPaymentMethodObject) error {
@@ -89,7 +139,7 @@ func (p *paymentUseCase) ProcessPaymentMethodWebhook(ctx context.Context, pm *yo
 
 	userID, err := p.cacheRepo.GetUserIDByPaymentID(ctx, pm.ID)
 	if err != nil {
-		return fmt.Errorf("failed to find user_id for payment_method %s: %w", pm.ID, err)
+		return errutil.Wrap("failed to find user_id for payment_method in cache", err, codes.NotFound)
 	}
 
 	issuer := ""
@@ -108,7 +158,7 @@ func (p *paymentUseCase) ProcessPaymentMethodWebhook(ctx context.Context, pm *yo
 
 	_, err = p.paymentRepo.Create(ctx, domainPaymentMethod)
 	if err != nil {
-		return fmt.Errorf("failed to save payment method to db: %w", err)
+		return errutil.Wrap("failed to save payment method to db", err, codes.Internal)
 	}
 
 	_ = p.cacheRepo.DeletePendingBinding(ctx, pm.ID)
@@ -131,9 +181,9 @@ func (p *paymentUseCase) ProcessPaymentWebhook(ctx context.Context, payment *yoo
 		return nil
 	}
 
-	err := p.orderRepo.UpdateStatusByPaymentID(ctx, payment.ID, newStatus)
+	err := p.orderClient.UpdateOrderStatus(ctx, payment.ID, newStatus)
 	if err != nil {
-		return err
+		return errutil.Wrap("failed to notify order service about payment status", err, codes.Internal)
 	}
 
 	return nil
