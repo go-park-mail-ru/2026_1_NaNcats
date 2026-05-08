b9af946ee4f06a52a91e14783db1a393d9797b45
KEI3AB <ramazanoff06@yandex.ru>
2026-05-08 22:41:38 +0300

    Обновил слой репозитория под новую таблицу идемпотентности в БД, реализовал новый метод для проверки адреса на существование

--- services/address/internal/repository/address.go
+++ services/address/internal/repository/address.go
@@ -10,7 +10,8 @@ import (
 type AddressRepository interface {
 	CreateAddress(ctx context.Context, userID int64, addr domain.Address, idempotencyKey string) (string, error)
 	GetAddressesByUserID(ctx context.Context, userID int64) ([]domain.Address, error)
-	UpdateAddress(ctx context.Context, userID int64, addr domain.Address) error
-	DeleteAddress(ctx context.Context, userID int64, publicID string) error
+	UpdateAddress(ctx context.Context, userID int64, addr domain.Address, idempotencyKey string) error
+	DeleteAddress(ctx context.Context, userID int64, publicID string, idempotencyKey string) error
 	GetInternalIDByPublicID(ctx context.Context, userID int64, publicID string) (int, error)
+	CheckAddressExists(ctx context.Context, userID int64, publicID string) error
 }
--- services/address/internal/repository/postgres/address_repo.go
+++ services/address/internal/repository/postgres/address_repo.go
@@ -1,13 +1,17 @@
 package postgres
 
+//go:generate easyjson $GOFILE
+
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/go-park-mail-ru/2026_1_NaNcats/services/address/internal/domain"
 	"github.com/go-park-mail-ru/2026_1_NaNcats/services/address/internal/repository"
 	"github.com/go-park-mail-ru/2026_1_NaNcats/shared/pkg/postgres"
 	"github.com/jackc/pgx/v5"
+	"github.com/mailru/easyjson"
 )
 
 type addressDB struct {
@@ -40,6 +44,11 @@ func (a addressDB) toDomain() domain.Address {
 	}
 }
 
+//easyjson:json
+type idempotencyResponse struct {
+	PublicID string `json:"public_id,omitempty"`
+}
+
 type addressRepo struct {
 	pool postgres.PgxPool
 }
@@ -55,18 +64,46 @@ func (r *addressRepo) CreateAddress(ctx context.Context, userID int64, addr doma
 	}
 	defer tx.Rollback(ctx)
 
+	var payloadBytes []byte
+	err = tx.QueryRow(ctx, `
+		INSERT INTO "idempotency_records" (user_id, idempotency_key, grpc_method)
+		VALUES ($1, $2, 'CreateAddress')
+		ON CONFLICT (user_id, idempotency_key) DO NOTHING
+		RETURNING response_payload;
+	`, userID, idempotencyKey).Scan(&payloadBytes)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			err = tx.QueryRow(ctx, `
+				SELECT response_payload FROM "idempotency_records" 
+				WHERE user_id = $1 AND idempotency_key = $2
+			`, userID, idempotencyKey).Scan(&payloadBytes)
+			if err != nil {
+				return "", err
+			}
+
+			if payloadBytes == nil {
+				return "", fmt.Errorf("request is already in progress")
+			}
+
+			var savedResp idempotencyResponse
+			if err := easyjson.Unmarshal(payloadBytes, &savedResp); err != nil {
+				return "", err
+			}
+			return savedResp.PublicID, nil
+		}
+		return "", err
+	}
+
 	locationQuery := `
-		INSERT INTO "location" (address_text, coordinate, idempotency_key)
-		VALUES ($1, ST_SetSRID(ST_MakePoint($2, $3), 4326), $4)
-		ON CONFLICT (idempotency_key) DO UPDATE
-		SET idempotency_key = EXCLUDED.idempotency_key
-		RETURNING id;`
+		INSERT INTO "location" (address_text, coordinate)
+		VALUES ($1, ST_SetSRID(ST_MakePoint($2, $3), 4326))
+		RETURNING id;
+		`
 
 	clientAddressQuery := `
-		INSERT INTO "client_address" (location_id, client_account_id, apartment, entrance, floor_level, door_code, courier_comment, label, idempotency_key)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-		ON CONFLICT (idempotency_key) DO UPDATE
-		SET idempotency_key = EXCLUDED.idempotency_key
+		INSERT INTO "client_address" (location_id, client_account_id, apartment, entrance, floor_level, door_code, courier_comment, label)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
 		RETURNING public_id;
 		`
 
@@ -74,8 +111,7 @@ func (r *addressRepo) CreateAddress(ctx context.Context, userID int64, addr doma
 	err = tx.QueryRow(ctx, locationQuery,
 		addr.Location.AddressText,
 		addr.Location.Longitude,
-		addr.Location.Latitude,
-		idempotencyKey).Scan(&locationID)
+		addr.Location.Latitude).Scan(&locationID)
 	if err != nil {
 		return "", fmt.Errorf("insert location failed: %w", err)
 	}
@@ -89,10 +125,19 @@ func (r *addressRepo) CreateAddress(ctx context.Context, userID int64, addr doma
 		addr.Floor,
 		addr.DoorCode,
 		addr.CourierComment,
-		addr.Label,
-		idempotencyKey).Scan(&addressPublicID)
+		addr.Label).Scan(&addressPublicID)
 	if err != nil {
 		return "", fmt.Errorf("insert address failed: %w", err)
+	}
+
+	respData, _ := easyjson.Marshal(idempotencyResponse{PublicID: addressPublicID})
+	_, err = tx.Exec(ctx, `
+		UPDATE "idempotency_records" 
+		SET response_payload = $1 
+		WHERE user_id = $2 AND idempotency_key = $3
+	`, respData, userID, idempotencyKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to update idempotency payload: %w", err)
 	}
 
 	return addressPublicID, tx.Commit(ctx)
@@ -135,47 +180,104 @@ func (r *addressRepo) GetAddressesByUserID(ctx context.Context, userID int64) ([
 	return result, nil
 }
 
-func (r *addressRepo) DeleteAddress(ctx context.Context, userID int64, publicID string) error {
-	query := `
-		UPDATE "client_address"
-		SET is_active = false
-		WHERE public_id = $1 AND client_account_id = $2;
-	`
-
-	res, err := r.pool.Exec(ctx, query, publicID, userID)
-	if err != nil {
-		return err
-	}
-	if res.RowsAffected() == 0 {
-		return domain.ErrNoChangesProvided
-	}
-
-	return nil
-}
-
-func (r *addressRepo) UpdateAddress(ctx context.Context, userID int64, addr domain.Address) error {
+func (r *addressRepo) DeleteAddress(ctx context.Context, userID int64, publicID, idempotencyKey string) error {
 	tx, err := r.pool.Begin(ctx)
 	if err != nil {
 		return err
 	}
 	defer tx.Rollback(ctx)
 
+	var payloadBytes []byte
+	err = tx.QueryRow(ctx, `
+		INSERT INTO "idempotency_records" (user_id, idempotency_key, grpc_method)
+		VALUES ($1, $2, 'DeleteAddress')
+		ON CONFLICT (user_id, idempotency_key) DO NOTHING
+		RETURNING response_payload;
+	`, userID, idempotencyKey).Scan(&payloadBytes)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			// ретрай
+			return nil
+		}
+		return fmt.Errorf("failed to insert idempotency record: %w", err)
+	}
+
+	query := `
+		UPDATE "client_address"
+		SET is_active = false
+		WHERE public_id = $1 AND client_account_id = $2 AND is_active = true;
+	`
+	res, err := tx.Exec(ctx, query, publicID, userID)
+	if err != nil {
+		return fmt.Errorf("failed to delete address: %w", err)
+	}
+	if res.RowsAffected() == 0 {
+		_, _ = tx.Exec(ctx, `DELETE FROM "idempotency_records" WHERE user_id = $1 AND idempotency_key = $2`, userID, idempotencyKey)
+		return domain.ErrAddressNotFound
+	}
+
+	_, err = tx.Exec(ctx, `
+		UPDATE "idempotency_records" 
+		SET response_payload = '{}'::jsonb 
+		WHERE user_id = $1 AND idempotency_key = $2
+	`, userID, idempotencyKey)
+	if err != nil {
+		return fmt.Errorf("failed to update idempotency payload: %w", err)
+	}
+
+	return tx.Commit(ctx)
+}
+
+func (r *addressRepo) UpdateAddress(ctx context.Context, userID int64, addr domain.Address, idempotencyKey string) error {
+	tx, err := r.pool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback(ctx)
+
+	var payloadBytes []byte
+	err = tx.QueryRow(ctx, `
+		INSERT INTO "idempotency_records" (user_id, idempotency_key, grpc_method)
+		VALUES ($1, $2, 'UpdateAddress')
+		ON CONFLICT (user_id, idempotency_key) DO NOTHING
+		RETURNING response_payload;
+	`, userID, idempotencyKey).Scan(&payloadBytes)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			// ретрай
+			return nil
+		}
+		return fmt.Errorf("failed to insert idempotency record: %w", err)
+	}
+
+	var locationID int
+	err = tx.QueryRow(ctx, `
+		SELECT location_id FROM "client_address" 
+		WHERE public_id = $1 AND client_account_id = $2 AND is_active = true
+	`, addr.PublicID, userID).Scan(&locationID)
+
+	if err != nil {
+		_, _ = tx.Exec(ctx, `DELETE FROM "idempotency_records" WHERE user_id = $1 AND idempotency_key = $2`, userID, idempotencyKey)
+		if errors.Is(err, pgx.ErrNoRows) {
+			return domain.ErrAddressNotFound
+		}
+		return fmt.Errorf("failed to find address for update: %w", err)
+	}
+
 	queryLoc := `
 		UPDATE "location"
 		SET address_text = $1, 
-		    coordinate = ST_SetSRID(ST_MakePoint($2, $3), 4326)
-		WHERE id = (
-			SELECT location_id 
-			FROM "client_address" 
-			WHERE public_id = $4 AND client_account_id = $5
-		);`
+		    coordinate = ST_SetSRID(ST_MakePoint($2, $3), 4326),
+			updated_at = NOW()
+		WHERE id = $4;`
 
 	_, err = tx.Exec(ctx, queryLoc,
 		addr.Location.AddressText,
 		addr.Location.Longitude,
 		addr.Location.Latitude,
-		addr.PublicID,
-		userID,
+		locationID,
 	)
 	if err != nil {
 		return fmt.Errorf("update location failed: %w", err)
@@ -188,10 +290,11 @@ func (r *addressRepo) UpdateAddress(ctx context.Context, userID int64, addr doma
 		    floor_level = $3, 
 		    door_code = $4, 
 		    courier_comment = $5, 
-		    label = $6
+		    label = $6,
+			updated_at = NOW()
 		WHERE public_id = $7 AND client_account_id = $8;`
 
-	result, err := tx.Exec(ctx, queryAddr,
+	_, err = tx.Exec(ctx, queryAddr,
 		addr.Apartment,
 		addr.Entrance,
 		addr.Floor,
@@ -205,8 +308,13 @@ func (r *addressRepo) UpdateAddress(ctx context.Context, userID int64, addr doma
 		return fmt.Errorf("update client_address failed: %w", err)
 	}
 
-	if result.RowsAffected() == 0 {
-		return domain.ErrAddressNotFound
+	_, err = tx.Exec(ctx, `
+		UPDATE "idempotency_records" 
+		SET response_payload = '{}'::jsonb 
+		WHERE user_id = $1 AND idempotency_key = $2
+	`, userID, idempotencyKey)
+	if err != nil {
+		return fmt.Errorf("failed to update idempotency payload: %w", err)
 	}
 
 	return tx.Commit(ctx)
@@ -215,14 +323,33 @@ func (r *addressRepo) UpdateAddress(ctx context.Context, userID int64, addr doma
 func (r *addressRepo) GetInternalIDByPublicID(ctx context.Context, userID int64, publicID string) (int, error) {
 	query := `
 		SELECT id FROM "client_address"
-		WHERE public_id = $1 AND client_account_id = $2;
+		WHERE public_id = $1 AND client_account_id = $2 AND is_active = true;
 	`
 
 	var internalID int
 	err := r.pool.QueryRow(ctx, query, publicID, userID).Scan(&internalID)
 	if err != nil {
-		return 0, err
+		if errors.Is(err, pgx.ErrNoRows) {
+			return 0, domain.ErrAddressNotFound
+		}
+		return 0, fmt.Errorf("failed to get internal ID: %w", err)
 	}
 
-	return internalID, err
+	return internalID, nil
+}
+
+func (r *addressRepo) CheckAddressExists(ctx context.Context, userID int64, publicID string) error {
+	query := `
+		SELECT 1 FROM "client_address" 
+		WHERE client_account_id = $1 AND public_id = $2 AND is_active = true
+	`
+	var dummy int
+	err := r.pool.QueryRow(ctx, query, userID, publicID).Scan(&dummy)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return domain.ErrAddressNotFound
+		}
+		return fmt.Errorf("failed to check address existence: %w", err)
+	}
+	return nil
 }
