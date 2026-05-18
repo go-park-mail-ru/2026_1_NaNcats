8bea8ff00884d05a541a12785c82cf360f0fcf1f
KEI3AB <ramazanoff06@yandex.ru>
2026-05-18 18:24:18 +0300

    Прописал ошибки для 5 букв

--- services/user/internal/domain/errors.go
+++ services/user/internal/domain/errors.go
@@ -12,4 +12,11 @@ var (
 	ErrInvalidImageExt    = errutil.New("INVALID_EXTENSION", "invalid image extension", codes.InvalidArgument)
 	ErrImageNotFound      = errutil.New("IMAGE_NOT_FOUND", "image not found", codes.NotFound)
 	ErrNoChangesProvided  = errutil.New("NO_CHANGES", "no changes provided", codes.InvalidArgument)
+
+	ErrGameAlreadyFinished = errutil.New("GAME_ALREADY_FINISHED", "the game is already finished for today", codes.FailedPrecondition)
+	ErrWordNotInDictionary = errutil.New("WORD_NOT_IN_DICTIONARY", "word is not in the dictionary", codes.InvalidArgument)
+	ErrInvalidWordLength   = errutil.New("INVALID_WORD_LENGTH", "word length must be exactly 5 letters", codes.InvalidArgument)
+	ErrMaxAttemptsReached  = errutil.New("MAX_ATTEMPTS_REACHED", "max attempts reached", codes.FailedPrecondition)
+	ErrDailyWordNotFound   = errutil.New("DAILY_WORD_NOT_FOUND", "daily word not set for today", codes.Internal)
+	ErrIdempotencyConflict = errutil.New("IDEMPOTENCY_CONFLICT", "request with this idempotency key already processed", codes.AlreadyExists)
 )
