7e2efbf06c0c77ed4238118102f3969d45b7f2ed
KEI3AB <ramazanoff06@yandex.ru>
2026-05-18 18:56:50 +0300

    Реализовал на стороне Api Gateway клиент для работы с 5 букв.

---
+++ api-gateway/internal/grpc_client/gameclient/game.go
@@ -0,0 +1,71 @@
+package gameclient
+
+import (
+	"context"
+	"errors"
+	"strings"
+
+	pb "github.com/go-park-mail-ru/2026_1_NaNcats/shared/proto/user"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	ErrGameAlreadyFinished = errors.New("game already finished for today")
+	ErrWordNotInDictionary = errors.New("word is not in the dictionary")
+	ErrInvalidWordLength   = errors.New("invalid word length")
+	ErrIdempotencyConflict = errors.New("idempotency conflict: request already processed")
+	ErrInternal            = errors.New("internal server error")
+)
+
+//go:generate mockgen -destination=mocks/game_mock.go -package=mocks github.com/go-park-mail-ru/2026_1_NaNcats/api-gateway/internal/grpc_client/gameclient GameClient
+type GameClient interface {
+	GetDailyWordleState(ctx context.Context, userID int64) (*pb.GetDailyWordleStateResponse, error)
+	MakeWordleGuess(ctx context.Context, userID int64, guess, idempotencyKey string) (*pb.MakeWordleGuessResponse, error)
+}
+
+type gameClient struct {
+	client pb.WordleServiceClient
+}
+
+func NewGameClient(cl pb.WordleServiceClient) GameClient {
+	return &gameClient{client: cl}
+}
+
+func (c *gameClient) GetDailyWordleState(ctx context.Context, userID int64) (*pb.GetDailyWordleStateResponse, error) {
+	resp, err := c.client.GetDailyWordleState(ctx, &pb.GetDailyWordleStateRequest{
+		UserId: userID,
+	})
+	if err != nil {
+		return nil, ErrInternal
+	}
+	return resp, nil
+}
+
+func (c *gameClient) MakeWordleGuess(ctx context.Context, userID int64, guess, idempotencyKey string) (*pb.MakeWordleGuessResponse, error) {
+	resp, err := c.client.MakeWordleGuess(ctx, &pb.MakeWordleGuessRequest{
+		UserId:         userID,
+		Guess:          guess,
+		IdempotencyKey: idempotencyKey,
+	})
+
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok {
+			switch st.Code() {
+			case codes.AlreadyExists:
+				return nil, ErrIdempotencyConflict
+			case codes.FailedPrecondition:
+				return nil, ErrGameAlreadyFinished
+			case codes.InvalidArgument:
+				if strings.Contains(st.Message(), "dictionary") {
+					return nil, ErrWordNotInDictionary
+				}
+				return nil, ErrInvalidWordLength
+			}
+		}
+		return nil, ErrInternal
+	}
+
+	return resp, nil
+}
