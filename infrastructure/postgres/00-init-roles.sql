c9f34ff6ab2e26be07c52c92ffdbab613219fb0f
KEI3AB <ramazanoff06@yandex.ru>
2026-05-25 12:45:31 +0300

    добавил ещё роли

--- infrastructure/postgres/00-init-roles.sql
+++ infrastructure/postgres/00-init-roles.sql
@@ -1,14 +1,19 @@
-CREATE ROLE app_read_only;
 CREATE ROLE app_read_write;
-
-GRANT SELECT ON ALL TABLES IN SCHEMA public TO app_read_only;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO app_read_write;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO app_read_write;
 
-CREATE USER user_service_user WITH PASSWORD 'secure_password_1';
-CREATE USER order_service_user WITH PASSWORD 'secure_password_2';
-CREATE USER cart_service_user WITH PASSWORD 'secure_password_3';
+CREATE USER user_svc_user WITH PASSWORD 'user_pass_123';
+CREATE USER restaurant_svc_user WITH PASSWORD 'rest_pass_123';
+CREATE USER cart_svc_user WITH PASSWORD 'cart_pass_123';
+CREATE USER address_svc_user WITH PASSWORD 'addr_pass_123';
+CREATE USER payment_svc_user WITH PASSWORD 'pay_pass_123';
+CREATE USER order_svc_user WITH PASSWORD 'order_pass_123';
+CREATE USER support_svc_user WITH PASSWORD 'supp_pass_123';
 
-GRANT app_read_write TO user_service_user;
-GRANT app_read_write TO order_service_user;
-GRANT app_read_write TO cart_service_user;
+GRANT app_read_write TO user_svc_user;
+GRANT app_read_write TO restaurant_svc_user;
+GRANT app_read_write TO cart_svc_user;
+GRANT app_read_write TO address_svc_user;
+GRANT app_read_write TO payment_svc_user;
+GRANT app_read_write TO order_svc_user;
+GRANT app_read_write TO support_svc_user;
